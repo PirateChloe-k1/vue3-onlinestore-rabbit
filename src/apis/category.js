acc7c4db89713316c11ee2bd1f0b1494ef5b71a1
root <piratechloe04@outlook.com>
2025-08-04 15:21:59 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,3 +1,4 @@
+import httpInstance from '@/utils/http'
 import request from '@/utils/http'
 
 /**
@@ -8,6 +9,20 @@ import request from '@/utils/http'
 export const getCategoryAPI = (id) => {
     return request({
         url: '/category',
+        params: {
+            id
+        }
+    })
+}
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+    return httpInstance({
+        url: '/category/sub/filter',
         params: {
             id
         }
