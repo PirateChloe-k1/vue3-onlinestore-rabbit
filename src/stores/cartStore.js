29db3171348e5f9c9aa09be5b53884f0c8c5bc2b
root <piratechloe04@outlook.com>
2025-08-08 03:10:27 +0800

    本地购物车-购物车列表-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -32,9 +32,9 @@ export const useCartStore = defineStore('cart', () => {
 
     // 单选功能
     const singleCheck = (skuId, selected) => {
-    // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
-    const item = cartList.value.find((item) => item.skuId === skuId)
-    item.selected = selected
+        // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+        const item = cartList.value.find((item) => item.skuId === skuId)
+        item.selected = selected
     }
 
 
@@ -44,13 +44,24 @@ export const useCartStore = defineStore('cart', () => {
     // 2. 总价 所有项的count*price之和
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
+    // 全选功能action
+    const allCheck = (selected) => {
+        // 把cartList中的每一项的selected都设置为当前的全选框状态
+        cartList.value.forEach(item => item.selected = selected)
+    }
+
+    // 是否全选计算属性
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
+
     return {
         cartList,
         allCount,
         allPrice,
+        isAll,
         addCart,
         delCart,
-        singleCheck
+        singleCheck,
+        allCheck
     }
 
 
