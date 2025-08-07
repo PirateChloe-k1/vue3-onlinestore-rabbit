f83d06bea865306a73e42fedc1f7bf2c4acd53ad
root <piratechloe04@outlook.com>
2025-08-07 23:04:13 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from "element-plus";
 import { useUserStore } from "@/stores/user";
+import router from "@/router";
 
 const httpInstance = axios.create({
     //根域名
@@ -27,10 +28,18 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  //  401token失效处理
+  //  1.清除本地用户数据
+  //  2.跳转到登录页面
+  if(e.response.status === 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
