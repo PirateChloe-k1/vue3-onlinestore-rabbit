a8529eb6b0a5475ef354b343b062c4824b7fa25c
root <piratechloe04@outlook.com>
2025-08-07 18:39:10 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from "element-plus";
+import { useUserStore } from "@/stores/user";
 
 const httpInstance = axios.create({
     //根域名
@@ -14,6 +15,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  //1.从pinia获取数据
+  const userStore = useUserStore()
+  //2.按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
