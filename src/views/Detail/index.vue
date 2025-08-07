730c5fbf6ae2c4ead8b7e7acf0890beca1a11de7
root <piratechloe04@outlook.com>
2025-08-07 23:52:19 +0800

    本地购物车-添加购物车实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -15,6 +15,38 @@ onMounted(() => {
     getGoods()
 })
 
+//sku规格被操作时
+let skuObj = {}
+const skuChange = (sku) => {
+    console.log(sku);
+    skuObj = sku
+}
+
+//count
+const count = ref(1)
+const countChange = (count) => {
+    console.log(count);
+}
+//添加购物车
+const addCart = () => {
+    if (skuObj.skuId) {
+        //规格已经选择 触发action
+        cartStore.addCart({
+            id: goods.value.id,
+            name: goods.value.name,
+            picture: goods.value.mainPictures[0],
+            price: goods.value.price,
+            count: count.value,
+            skuId: skuObj.skuId,
+            attrsText: skuObj.specsText,
+            selected: true
+        })
+    } else {
+        //规格没有选择 提示用户
+        ElMessage.warning('请选择规格')
+    }
+}
+
 </script>
 
 <template>
@@ -92,10 +124,10 @@ onMounted(() => {
                             <!-- sku组件 -->
                             <XtxSku :goods="goods" @change="skuChange"/>
                             <!-- 数据组件 -->
-
+                            <el-input-number :min="1" v-model="count" @change="countChange" />
                             <!-- 按钮组件 -->
                             <div>
-                                <el-button size="large" class="btn">
+                                <el-button size="large" class="btn" @click="addCart">
                                     加入购物车
                                 </el-button>
                             </div>
