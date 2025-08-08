9f25bd492213f7795bdfae9dd644358ee27401dc
root <piratechloe04@outlook.com>
2025-08-08 17:01:49 +0800

    接口购物车-删除功能&小优化

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -2,8 +2,8 @@
 
 import { ref,computed } from 'vue'
 import { defineStore } from 'pinia'
-import { useUserStore } from './user'
-import {insertCartAPI,findNewCartListAPI} from '@/apis/cart'
+import { useUserStore } from './userStore'
+import {insertCartAPI,findNewCartListAPI,delCartAPI} from '@/apis/cart'
 
 export const useCartStore = defineStore('cart', () => {
     const userStore = useUserStore()
@@ -11,14 +11,18 @@ export const useCartStore = defineStore('cart', () => {
 
     //1.定义state - cartList
     const cartList = ref([])
+    // 获取最新购物车action
+    const updateNewList = async () => {
+        const res = await findNewCartListAPI()
+        cartList.value = res.result
+    }
     //2.定义action - addCart
     const addCart = async (goods) => {
         if(isLogin.value){
             const {skuId,count} = goods
             // 登录之后的加入购物车逻辑
             await insertCartAPI({skuId,count})
-            const res = findNewCartListAPI()
-            cartList.value = res.result
+            updateNewList()
         }else{
         // 添加购物车操作
         // 已添加过 - count + 1
@@ -37,11 +41,17 @@ export const useCartStore = defineStore('cart', () => {
 
     // 删除购物车
     const delCart = async (skuId) => {
-        // 思路：
-        // 1. 找到要删除项的下标值 - splice
-        // 2. 使用数组的过滤方法 - filter
-        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
-        cartList.value.splice(idx, 1)
+        if(isLogin.value){
+            // 调用接口实现接口购物车的删除功能
+            await delCartAPI([skuId])
+            updateNewList()
+        }else{
+            // 思路：
+            // 1. 找到要删除项的下标值 - splice
+            // 2. 使用数组的过滤方法 - filter
+            const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+            cartList.value.splice(idx, 1)
+        }
     }
 
     // 单选功能
